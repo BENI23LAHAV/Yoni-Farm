4179b95d1f866b49b14a7f7d83c082731a7b6444
BENI23LAHAV <beni0548472300@gmail.com>
2024-10-10 11:16:36 +0300

    start with the responsive phone mode

--- src/Components/TeamFarm.jsx
+++ src/Components/TeamFarm.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+
+const rightArrow = require("../Images/right-arrow.png");
+const leftArrow = require("../Images/left-arrow.png");
 
 const TeamFarm = () => {
-  return <div>TeamFarm</div>;
+  const [farmTeams, setFarmTeams] = useState([
+    {
+      name: "יוני להב",
+      title: "מנהל ראשי ומדריך",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmV7bjGEmOGta3bXydiPduJUWNE9LKa2vVzQ&s",
+    },
+    {
+      name: "דוד להב",
+      title: "  אחראי תחזוקה וסייס",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmV7bjGEmOGta3bXydiPduJUWNE9LKa2vVzQ&s",
+    },
+    {
+      name: "יהודה להב",
+      title: "  סייס",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmV7bjGEmOGta3bXydiPduJUWNE9LKa2vVzQ&s",
+    },
+  ]);
+  const [indexTeam, setIndexTeam] = useState(0);
+
+  return (
+    <div className="farm-team">
+      <h2>צוות החווה</h2>
+      <ul>
+        <li>
+          <img
+            className="arrow-img"
+            src={leftArrow}
+            alt="left arrow"
+            style={{ cursor: "pointer" }}
+            onClick={() => handleNext(setIndexTeam)}
+          />
+        </li>
+        {getCurrentImage(farmTeams, indexTeam).map((team, index) => (
+          <li>
+            <div className="team-images">
+              <img src={team.image} alt={`${team.name}image`} />
+              <h4>{team.name}</h4>
+              <p>{team.title}</p>
+            </div>
+          </li>
+        ))}
+        <li>
+          <img
+            className="arrow-img"
+            src={rightArrow}
+            alt="right arrow"
+            style={{ cursor: "pointer" }}
+            onClick={() => handlePrev(setIndexTeam)}
+          />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default TeamFarm;
+
+const getCurrentImage = (arr, index) => {
+  const result = [];
+  for (let i = 0; i < 3; i++) {
+    const currentIndex = (Math.abs(index) + i) % arr.length;
+    result.push(arr[currentIndex]);
+  }
+  return result;
+};
+const handleNext = (setIndex) => {
+  setIndex((prev) => prev + 1);
+};
+const handlePrev = (setIndex) => {
+  setIndex((prev) => prev - 1);
+};

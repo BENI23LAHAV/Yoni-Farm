480d9cc6e2d16aaf42610db14f184b6842d52243
BENI23LAHAV <beni0548472300@gmail.com>
2024-12-05 14:30:01 +0200

    starting deploy website

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
         <ul>
           <li>
             <a
-              href="https://waze.com/ul/hsv8v15b17"
+              href="https://www.waze.com/he/live-map/directions?to=ll.31.83852249%2C34.84880877"
               target="_blank"
               rel="noreferrer">
               <img src={wazeImage} alt="waze Image" />
@@ -31,22 +31,25 @@ const Footer = () => {
               <img src={phoneImage} alt="phone Image" />
             </a>
           </li>
-          <li>
+          {/* <li>
             <a
               href="https://wa.me/+97254123456"
               target="_blank"
               rel="noreferrer">
               <img src={whatsappImage} alt="whatsapp Image" />
             </a>
-          </li>
+          </li> */}
           <li>
-            <a href="" target="_blank" rel="noreferrer">
+            <a
+              href="https://www.facebook.com/people/%D7%94%D7%97%D7%95%D7%95%D7%94-%D7%A9%D7%9C-%D7%97%D7%9F/61568537592902/"
+              target="_blank"
+              rel="noreferrer">
               <img src={facebookImage} alt="facebook Image" />
             </a>
           </li>
           <li>
             <a
-              href="mailto:hachva@example.com"
+              href="mailto:x0527639398@gmail.com"
               target="_blank"
               rel="noreferrer">
               <img src={gmailImage} alt="gmail Image" />
@@ -64,7 +67,7 @@ const Footer = () => {
           </ul>
         </div>
         <div className="footer-copyright-right">
-          <p>© 2024.  כל הזכויות שמורות.</p>
+          <p>© 2024. כל הזכויות שמורות.</p>
           <p>Created & Desigend by: Beni Lahav</p>
           <a
             href="mailto:beni0548472300@gmail.com"
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,22 +9,35 @@ import {
 } from "../Components/Sourses";
 
 const Home = () => {
+  const [visible, setVisible] = useState(false);
+  const [currentImage, setCurrentImage] = useState("");
+
+  const handleImageClick = (image) => {
+    console.log("currentImage", currentImage);
+    console.log(visible);
+
+    setCurrentImage(image);
+    setVisible(true);
+  };
+  const handleClose = () => {
+    setVisible(false);
+    setCurrentImage("");
+  };
+
   const [information, setInformation] = useState(
     "החווה של חן ממוקמת ביישוב מזכרת בתיה באיזור השפלה.בחווה תוכלו למצוא את מיטב המדריכים המנוסים שלנו. עשרות ילדים אשר חווים את הרכיבה באופן חווייתי מידי שבוע. תוכלו גם למצוא פינת חיי יפייפיה, מאהל בדואי אותנטי, ומרחבים מרחיבי דעת."
   );
   const [imagesIndex, setImagesIndex] = useState(
     Math.floor(Math.random() * images.length)
   );
-  // setInterval(() => {
-  //   handleNext(setImagesIndex);
-  // }, 2000);
+
   const [opening, setOpening] = useState([
-    { day: "א", isOpen: true, from: "10:00", to: "22:00" },
-    { day: "ב", isOpen: true, from: "10:00", to: "22:00" },
-    { day: "ג", isOpen: true, from: "10:00", to: "22:00" },
-    { day: "ד", isOpen: true, from: "10:00", to: "22:00" },
-    { day: "ה", isOpen: true, from: "10:00", to: "22:00" },
-    { day: "ו", isOpen: true, from: "10:00", to: "14:00" },
+    { day: "א", isOpen: true, from: "09:00", to: "19:00" },
+    { day: "ב", isOpen: true, from: "09:00", to: "19:00" },
+    { day: "ג", isOpen: true, from: "09:00", to: "19:00" },
+    { day: "ד", isOpen: true, from: "09:00", to: "19:00" },
+    { day: "ה", isOpen: true, from: "09:00", to: "19:00" },
+    { day: "ו", isOpen: true, from: "09:00", to: "14:00" },
     { day: "שבת", isOpen: false, from: "00:00", to: "00:00" },
   ]);
   if (false) {
@@ -44,6 +57,7 @@ const Home = () => {
             src={item}
             key={index}
             className="home-image"
+            onClick={() => handleImageClick(item)}
           />
         ))}
       </div>
@@ -60,6 +74,65 @@ const Home = () => {
           ))}
         </ul>
       </div>
+      {visible && (
+        <>
+          <div
+            className="overlay"
+            onClick={handleClose} // הוספת אירוע לחיצה על ה-overlay
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0, 0, 0, 0.7)",
+              zIndex: 999,
+            }}></div>
+          <div
+            className="modal"
+            style={{
+              position: "fixed",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              backgroundColor: "white",
+              borderRadius: "8px",
+              boxShadow: "0 4px 10px rgba(0, 0, 0, 0.25)",
+              zIndex: 1000,
+            }}>
+            <div
+              className="modal-content"
+              style={{
+                padding: "20px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <span
+                className="close"
+                onClick={handleClose}
+                style={{
+                  position: "absolute",
+                  top: "10px",
+                  right: "10px",
+                  fontSize: "1.5rem",
+                  cursor: "pointer",
+                }}>
+                &times;
+              </span>
+              <img
+                src={currentImage}
+                alt="Modal"
+                style={{
+                  maxWidth: "100%",
+                  maxHeight: "80vh",
+                  objectFit: "contain",
+                }}
+              />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
